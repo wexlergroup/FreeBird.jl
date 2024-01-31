f3a650bb465602cda52d6538451e976a54a34b9a
Ray Yang <mingrui.yang@outlook.com>
2024-01-31 14:46:09 -0600

    unitfy LJ params and energy evaluation

--- src/EnergyEval.jl
+++ src/EnergyEval.jl
@@ -1,60 +1,67 @@
 module EnergyEval
 
 using AtomsBase
-using LinearAlgebra
+using Unitful
+using StaticArrays
 using ..Potentials
 
 export free_free_energy, frozen_energy, free_frozen_energy
 export interaction_energy, total_energy
 
-function free_free_energy(at::FlexibleSystem, lj::LJParameters; frozen::Int64=0)
-    free_free_energy = 0.0
+function pbc_dist(pos1::Union{SVector{T},Vector{T}},
+                      pos2::Union{SVector{T},Vector{T}},
+                      at::AbstractSystem) where {T}
+    pbc = at.boundary_conditions
+    box = at.bounding_box
+    distsq = 0.0u"Å"^2
+    for i in eachindex(pos1)
+        if pbc[i] == Periodic()
+            distsq += min(abs(pos1[i] - pos2[i]), box[i][i] - abs(pos1[i] - pos2[i]))^2
+        elseif pbc[i] == DirichletZero()
+            distsq += (pos1[i] - pos2[i])^2
+        else
+            error("Unsupported boundary condition: $(pbc[i])")
+        end
+    end
+    return sqrt(distsq)
+end
+
+function free_free_energy(at::AbstractSystem, lj::LJParameters; frozen::Int64=0)
+    free_free_energy = 0.0u"eV"
     for i in (frozen+1):length(at)
         for j in (i+1):length(at)
-            r = norm(at.particles[i][:position] - at.particles[j][:position]).val
+            r = pbc_dist(position(at, i), position(at, j), at)
             free_free_energy += lj_energy(r,lj)
         end
     end
     return free_free_energy
 end
 
-function frozen_energy(at::FlexibleSystem, lj::LJParameters, frozen::Int64)
-    frozen_energy = 0.0
+function frozen_energy(at::AbstractSystem, lj::LJParameters, frozen::Int64)
+    frozen_energy = 0.0u"eV"
     for i in 1:frozen
         for j in (i+1):frozen
-            r = norm(at.particles[i][:position] - at.particles[j][:position]).val
+            r = pbc_dist(position(at, i), position(at, j), at)
             frozen_energy += lj_energy(r,lj)
         end
     end
     return frozen_energy
 end
 
-function free_frozen_energy(at::FlexibleSystem, lj::LJParameters, frozen::Int64)
-    free_frozen_energy = 0.0
+function free_frozen_energy(at::AbstractSystem, lj::LJParameters, frozen::Int64)
+    free_frozen_energy = 0.0u"eV"
     for i in 1:frozen
         for j in (frozen+1):length(at)
-            r = norm(at.particles[i][:position] - at.particles[j][:position]).val
+            r = pbc_dist(position(at, i), position(at, j), at)
             free_frozen_energy += lj_energy(r,lj)
-            # @debug println("free_frozen_energy: ", free_frozen_energy, " r: ", r, " i and j: ", i, " ", j)
         end
     end
     return free_frozen_energy
 end
 
-function interaction_energy(at::FlexibleSystem, lj::LJParameters; frozen::Int64=0)
-    if frozen == 0
-        return free_free_energy(at, lj)
-    else
-        return free_free_energy(at, lj; frozen=frozen) + free_frozen_energy(at, lj, frozen)
-    end
-end
-
-function total_energy(at::FlexibleSystem, lj::LJParameters; frozen::Int64=0)
-    if frozen == 0
-        return free_free_energy(at, lj)
-    else
-        return interaction_energy(at, lj; frozen=frozen) + frozen_energy(at, lj, frozen)
-    end
+function interaction_energy(at::AbstractSystem, lj::LJParameters; frozen::Int64=0)
+    e_free_frozen = frozen == 0 ? 0.0u"eV" : free_frozen_energy(at, lj, frozen)
+    return free_free_energy(at, lj; frozen=frozen) + e_free_frozen
 end
 
 
--- src/Potentials.jl
+++ src/Potentials.jl
@@ -1,6 +1,7 @@
 module Potentials
 
-using Parameters
+# using Parameters
+using Unitful
 
 export LJParameters, lj_energy
 
@@ -16,14 +17,14 @@ A struct representing the parameters for the Lennard-Jones potential.
 - `shift::Float64`: The energy shift applied to the potential calculated at the cutoff distance.
 """
 struct LJParameters
-    epsilon::Float64
-    sigma::Float64
+    epsilon::typeof(1.0u"eV")
+    sigma::typeof(1.0u"Å")
     cutoff::Float64
-    shift::Float64
+    shift::typeof(0.0u"eV")
 end
 
 """
-    LJParameters(;epsilon=1.0, sigma=1.0, cutoff=Inf, shift=false)
+    LJParameters(;epsilon=1.0, sigma=1.0, cutoff=Inf, shift=true)
 A constructor for the LJParameters struct with default values for the 
 Lennard-Jones potential with no cutoff or shift. The `shift` parameter can be 
 specified as a boolean, if `true`, the shift energy is calculated automatically 
@@ -33,47 +34,60 @@ at the cutoff distance; or as a `Float64`, in which case the value is used direc
 
 ```jldoctest
 julia> lj = LJParameters(epsilon=0.1,sigma=2.5,cutoff=3.5,shift=false)
-LJParameters(0.1, 2.5, 3.5, 0.0)
+LJParameters(0.1 eV, 2.5 Å, 3.5, 0.0 eV)
 
 julia> lj = LJParameters(sigma=2.5)
-LJParameters(1.0, 2.5, Inf, 0.0)
+LJParameters(1.0 eV, 2.5 Å, Inf, 0.0 eV)
 
 julia> lj = LJParameters(cutoff=3.5,shift=5.0)
-LJParameters(1.0, 1.0, 3.5, 5.0)
+LJParameters(1.0 eV, 1.0 Å, 3.5, 5.0 eV)
 
 julia> lj = LJParameters(cutoff=3.5,shift=true)
-LJParameters(1.0, 1.0, 3.5, -0.0021747803916549904)
+LJParameters(1.0 eV, 1.0 Å, 3.5, -0.0021747803916549904 eV)
 
 julia> lj = LJParameters(cutoff=3.5,shift=false)
-LJParameters(1.0, 1.0, 3.5, 0.0)
+LJParameters(1.0 eV, 1.0 Å, 3.5, 0.0 eV)
 
 ```
 """
 function LJParameters(;epsilon=1.0, sigma=1.0, cutoff=Inf, shift=true)
     if isa(shift,Bool) && shift
-        shiftenergy = lj_energy(Float64(epsilon), Float64(sigma), Float64(cutoff))
-        
+        shiftenergy = lj_energy(Float64(epsilon)*u"eV", Float64(sigma)*u"Å", cutoff*Float64(sigma)*u"Å")  
     else
-        shiftenergy = Float64(shift)
+        shiftenergy = Float64(shift)*u"eV"
     end
-    return LJParameters(Float64(epsilon), Float64(sigma), Float64(cutoff), shiftenergy)
+    return LJParameters(epsilon*u"eV", sigma*u"Å", cutoff, shiftenergy)
 end
 
-function lj_energy(sigma::Float64, epsilon::Float64, r::Float64)
+"""
+    lj_energy(epsilon::eV, sigma::Å, r::Å)
+
+Compute the Lennard-Jones potential energy between two particles.
+
+Arguments:
+- `epsilon`: the energy scale of the potential (in electron volts)
+- `sigma`: the distance scale of the potential (in angstroms)
+- `r`: the distance between the particles (in angstroms)
+
+Returns:
+- The Lennard-Jones potential energy between the particles.
+
+"""
+function lj_energy(epsilon::typeof(1.0u"eV"), sigma::typeof(1.0u"Å"), r::typeof(1.0u"Å"))
     r6 = (sigma / r)^6
     r12 = r6^2
     return 4 * epsilon * (r12 - r6)
 end
 
 """
-    lj_energy(r::Float64, lj::LJParameters)
+    lj_energy(r::Å, lj::LJParameters)
 Calculate the Lennard-Jones energy for a given distance `r` and LJParameters `lj`.
 """
-function lj_energy(r::Float64, lj::LJParameters)
+function lj_energy(r::typeof(1.0u"Å"), lj::LJParameters)
     if r > lj.cutoff * lj.sigma
         return 0.0
     else
-        return lj_energy(lj.sigma, lj.epsilon, r) - lj.shift
+        return lj_energy(lj.epsilon, lj.sigma, r) - lj.shift
     end
 end
 

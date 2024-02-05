0c488a151948814777511bcaf42acba1edabf0d7
Ray Yang <mingrui.yang@outlook.com>
2024-02-05 17:59:21 -0600

    Add write_single_walker function to convert and save AtomWalker as AbstractSystem

--- src/FreeBirdIO.jl
+++ src/FreeBirdIO.jl
@@ -3,6 +3,8 @@ module FreeBirdIO
 using AtomsIO, ExtXYZ, DataFrames, CSV
 using AtomsBase, Unitful
 
+using ..AbstractWalkers
+
 export read_walkers, write_walkers
 export read_single_walker, write_single_walker
 export write_df
@@ -14,9 +16,24 @@ write_walkers(filename::String, ats::Vector) = save_trajectory(filename::String,
 
 read_single_walker(filename::String) = FastSystem(Atoms(read_frame(filename::String)))
 
-write_single_walker(filename::String, at::AbstractSystem) = save_system(filename::String, at)
+# write_single_walker(filename::String, at::AbstractSystem) = save_system(filename::String, at)
 
 write_df(filename::String, df::DataFrame) = CSV.write(filename, df; append=true)
+
+function convert_walker_to_system(at::AtomWalker)
+    config::FastSystem = at.configuration
+    energy = at.energy.val
+    iter = at.iter
+    num = at.num_frozen_part
+    e_frozen = at.energy_frozen_part.val
+    flex = AbstractSystem(config; energy=energy, iter=iter, num_frozen_part=num, energy_frozen_part=e_frozen)
+    return flex
+end
+
+function write_single_walker(filename::String, at::AtomWalker)
+    flex = convert_walker_to_system(at)
+    save_system(filename::String, flex)
+end
 
 
 function generate_random_starting_config(volume_per_particle::Float64, num_particle::Int; particle_type::Symbol=:H)

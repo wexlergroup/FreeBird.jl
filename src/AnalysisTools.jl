445c3c953f0c83f73ef74fa030b47fd9ed582d85
wexler <wexler@wustl.edu>
2024-05-15 14:49:42 -0500

    Changed ω_i in ωᵢ function to ωi for consistency with other functions in the AnalysisTools.jl module

--- src/AnalysisTools.jl
+++ src/AnalysisTools.jl
@@ -39,8 +39,8 @@ where \$N\$ is the number of walkers and \$i\$ is the iteration number.
 - A vector of \$\\omega\$ factors.
 """
 function ωᵢ(iters::Vector{Int}, n_walkers::Int)
-    ω_i = (1/(n_walkers+1))*(n_walkers/(n_walkers+1)).^iters
-    return ω_i
+    ωi = (1/(n_walkers+1))*(n_walkers/(n_walkers+1)).^iters
+    return ωi
 end
 
 """

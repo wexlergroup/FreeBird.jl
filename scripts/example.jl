6044001d644cafe92fd4d61723201a8b4fc57f0f
Ray Yang <mingrui.yang@outlook.com>
2024-07-09 10:31:22 -0500

    Hotfix/broken-examples (#45)

--- scripts/example.jl
+++ scripts/example.jl
@@ -2,26 +2,28 @@
 using FreeBird
 
 # First, we need to read the walkers from a file
-ats = read_walkers("scripts/slab-08-4x4.extxyz",pbc="TTF")
+ats = read_walkers("scripts/slab.extxyz",pbc="TTF")
 # Note that the periodic boundary conditions are set to True, True, False
 
 # Then, we need to create a potential object
-lj = LJParameters(epsilon=0.1, sigma=2.5, cutoff=4.0, shift=0.5)
+lj = LJParameters(epsilon=0.1, sigma=2.5, cutoff=4.0, shift=true)
 
 # Now, we can create the liveset by combining the walkers and the potential
-ls = LJAtomWalkers(ats[1:10], lj, 80)
+ls = LJAtomWalkers(ats, lj, 12)
 # The last argument is the number of frozen particles
 
 # We can now create a Monte Carlo object
-mc = MCDemonWalk(max_add_steps=10000, e_demon_tolerance=0.01u"eV")
-mix = MixedMCRoutine(main_routine=MCRandomWalk(),back_up_routine=mc)
+mc = MCRandomWalkClone()
+
+# Setting the parameters for the nested sampling
+ns_params = NestedSamplingParameters(1600, 0.1, 0.01, 1e-5, 1.0, 0, 200)
 
 # We can also create a save object
-save = SaveEveryN("output-energies.csv",100)
+save = SaveEveryN(n=1000)
 
-# And finally, we can run the simulation
-energies, ls, _ = nested_sampling_loop!(ls, 10000, mix, save)
+# And finally, we can run the simulation (takes about 1 minute)
+energies, ls, _ = nested_sampling_loop!(ls, ns_params, 10_000, mc, save)
 
 # The energies are saved in the file output-energies.csv
 # We can also save the final walkers to a file
-write_walkers("slab-08-final-ls.extxyz", ls.walkers)
+write_walkers("output-final-ls.extxyz", ls.walkers)

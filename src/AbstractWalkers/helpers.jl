0a8699bed4ffd8011054d7cd87c4f6c05bc098b5
Ray Yang <mingrui.yang@outlook.com>
2024-07-25 15:05:59 -0500

    refactor: Improve lattice visualization and sorting algorithm

--- src/AbstractWalkers/helpers.jl
+++ src/AbstractWalkers/helpers.jl
@@ -1,4 +1,57 @@
 # show methods
+
+"""
+    custom_sort(arr::Vector{Int}, period::Int)
+
+Sorts [1,2,3,4,5,6,7,8] into [1,3,5,7,2,4,6,8] for period = 2. Useful for printing triangular lattices.
+
+"""
+function custom_sort(arr::Vector{Int}, period::Int)
+    sections = length(arr) ÷ period
+    sorted_arr = deepcopy(arr)
+    for i in 1:sections
+        start = (i-1)*period + 1
+        stop = i*period
+        sub_arr = arr[start:stop]
+        odd_nums = filter(x -> x % 2 != 0, sub_arr)
+        even_nums = filter(x -> x % 2 == 0, sub_arr)
+        sorted_arr[start:stop] = vcat(odd_nums, even_nums)
+    end
+    return sorted_arr
+end
+
+function print_layer(io::IO, lattice::LatticeSystem{G}, boolvec::Vector{Bool}) where G
+    supercell_dimensions = lattice.supercell_dimensions
+    # set up zigzag indexing for triangular lattice
+    index = custom_sort(collect(1:length(boolvec)), lattice.supercell_dimensions[2]*4)
+    ind = 1
+    for i in 1:supercell_dimensions[1]
+        print(io, "      ")
+        if G == TriangularLattice
+            if isodd(i)
+                print(io, " ")
+            end
+            for j in 1:supercell_dimensions[2]*length(lattice.basis)
+                if boolvec[index[ind]]
+                    print(io, "● ")
+                else
+                    print(io, "○ ")
+                end
+                ind += 1
+            end
+        elseif G == SquareLattice
+            for j in 1:supercell_dimensions[2]*length(lattice.basis)
+                if boolvec[(i-1)*supercell_dimensions[2]*length(lattice.basis) + j]
+                    print(io, "● ")
+                else
+                    print(io, "○ ")
+                end
+            end
+        end
+        println(io)
+    end
+end
+
 function print_lattice(io::IO, lattice::LatticeSystem{G}, boolvec::Vector{Bool}) where G
     if G == GenericLattice
         print(io, boolvec)
@@ -6,41 +59,14 @@ function print_lattice(io::IO, lattice::LatticeSystem{G}, boolvec::Vector{Bool})
     end
     supercell_dimensions = lattice.supercell_dimensions
     if supercell_dimensions[3] == 1
-        for i in 1:supercell_dimensions[1]
-            print(io, "      ")
-            if G == TriangularLattice
-                if isodd(i)
-                    print(io, " ")
-                end
-            end
-            for j in 1:supercell_dimensions[2]
-                if boolvec[(i-1)*supercell_dimensions[2] + j]
-                    print(io, "● ")
-                else
-                    print(io, "○ ")
-                end
-            end
-            println(io)
-        end
+        print_layer(io, lattice, boolvec)
     else
         for k in 1:supercell_dimensions[3]
+            # slices the boolvec into layers
+            l = supercell_dimensions[1]*supercell_dimensions[2]*length(lattice.basis)
+            sec = ((k-1)*l+1):(k*l)
             println(io, "      Layer ", k)
-            for i in 1:supercell_dimensions[1]
-                print(io, "      ")
-                if G == TriangularLattice
-                    if isodd(i)
-                        print(io, " ")
-                    end
-                end
-                for j in 1:supercell_dimensions[2]
-                    if boolvec[(i-1)*supercell_dimensions[2] + j + (k-1)*supercell_dimensions[1]*supercell_dimensions[2]]
-                        print(io, "● ")
-                    else
-                        print(io, "○ ")
-                    end
-                end
-                println(io)
-            end
+            print_layer(io, lattice, boolvec[sec])
         end
     end
 end

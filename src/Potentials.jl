95b21b3a90868a0b979b122dc22ec1aa79aafd2d
Ray Yang <mingrui.yang@outlook.com>
2024-07-25 14:25:47 -0500

    Update src/Potentials.jl
    Co-authored-by: Robert Wexler <wexler@wustl.edu>

--- src/Potentials.jl
+++ src/Potentials.jl
@@ -176,7 +176,7 @@ the upper triangular part of the matrix is needed.
 function CompositeLJParameters(c::Int, ljs::Vector{LJParameters})
     if length(ljs) == c^2
         # If the number of LJParameters sets is equal to the number 
-        # of elements in the matrix, assuming that Vector{LJParameters} 
+        # of elements in the matrix, then assume that Vector{LJParameters} 
         # is a flattened matrix, then reshape the vector into a matrix.
         return CompositeLJParameters{c}(reshape(ljs, c, c))
     elseif length(ljs) == c*(c+1)/2

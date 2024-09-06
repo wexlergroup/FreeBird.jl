61492e2d2ce1971b52bb39f539188c42304f4fad
junchichen21 <c.junchi@wustl.edu>
2024-09-06 15:07:05 -0500

    updated the test code for Hamiltonians module since the module code has some changes.

--- test/test-Hamiltonians.jl
+++ test/test-Hamiltonians.jl
@@ -1,13 +1,17 @@
 @testset "Lattice-Gas Hamiltonian" begin
-    @test typeof(LGHamiltonian) == DataType
+    
+    @test typeof(LatticeGasHamiltonian) == DataType
 
-    lg_h = LGHamiltonian(1u"eV", 2.2u"eV",3.33u"eV")
+    lattice_gas_hamiltonian = LatticeGasHamiltonian(1u"eV", 2.2u"eV",3.33u"eV")
 
-    @test lg_h |> typeof == LGHamiltonian
-    @test lg_h.adsorption_energy == 1u"eV"
-    @test typeof(lg_h.adsorption_energy) == typeof(1.0u"eV")
-    @test lg_h.nn_interaction_energy == 2.2u"eV"
-    @test typeof(lg_h.nn_interaction_energy) == typeof(1.0u"eV")
-    @test lg_h.nnn_interaction_energy == 3.33u"eV"
-    @test typeof(lg_h.nnn_interaction_energy) == typeof(1.0u"eV")
+    @test lattice_gas_hamiltonian |> typeof == LatticeGasHamiltonian
+
+    @test lattice_gas_hamiltonian.adsorption_energy == 1u"eV"
+    @test typeof(lattice_gas_hamiltonian.adsorption_energy) == typeof(1.0u"eV")
+
+    @test lattice_gas_hamiltonian.nn_interaction_energy == 2.2u"eV"
+    @test typeof(lattice_gas_hamiltonian.nn_interaction_energy) == typeof(1.0u"eV")
+
+    @test lattice_gas_hamiltonian.nnn_interaction_energy == 3.33u"eV"
+    @test typeof(lattice_gas_hamiltonian.nnn_interaction_energy) == typeof(1.0u"eV")
 end 

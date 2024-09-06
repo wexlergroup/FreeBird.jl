079b17c73de041a35b0af5169ffceb9cbc59e68d
junchichen21 <c.junchi@wustl.edu>
2024-09-06 17:45:42 -0500

    modified the test code for pbc_dist(), where both periodic and non-periodic conditions are tested.
    added the test code for functions inter_component_energy() and intra_component_energy()

--- test/test-EnergyEval.jl
+++ test/test-EnergyEval.jl
@@ -1,11 +1,42 @@
 @testset "Energy Evaluation" begin
-    list = [:H => [0.2, 0.5, 0.5],:H => [0.8, 0.5, 0.5]]
+
+    coor_list1 = [:H => [0.2, 0.5, 0.5], :H => [0.8, 0.5, 0.5], :H => [0.5, 0.2, 0.5], :H => [0.5, 0.8, 0.5], :H => [0.5, 0.5, 0.2], :H => [0.5, 0.5, 0.8]]
+    coor_list2 = [:H => [0, 0, 0], :H => [0.5, 0.5, 0.5]]
     box = [[10.0, 0.0, 0.0], [0.0, 10.0, 0.0], [0.0, 0.0, 10.0]]u"Å"
-    sys = periodic_system(list,box,fractional=true)
-    pos1 = position(sys.particles[1])
-    pos2 = position(sys.particles[2])
-    @test pbc_dist(pos1, pos2, sys) == 4.0u"Å"
+
+    sys1 = periodic_system(coor_list1, box, fractional=true)
+    pos1 = position(sys1.particles[1])
+    pos2 = position(sys1.particles[2])
+
+    pos3 = position(sys1.particles[3])
+    pos4 = position(sys1.particles[4])
+    
+    pos5 = position(sys1.particles[5])
+    pos6 = position(sys1.particles[6])
+
+    # Test output distances in x, y, z directions under the periodic condition
+    @test pbc_dist(pos1, pos2, sys1) == 4.0u"Å" 
+    @test pbc_dist(pos3, pos4, sys1) == 4.0u"Å"
+    @test pbc_dist(pos5, pos6, sys1) == 4.0u"Å"
+
     boundary_conditions = [DirichletZero(), DirichletZero(), DirichletZero()]
-    sys = FlexibleSystem(sys; boundary_conditions=boundary_conditions)
-    @test pbc_dist(pos1, pos2, sys) == 6.0u"Å"
+    non_periodic_sys = FlexibleSystem(sys1; boundary_conditions=boundary_conditions)
+
+    # Test output distances in x, y, z directions under the non-periodic condition
+    @test pbc_dist(pos1, pos2, non_periodic_sys) == 6.0u"Å"
+    @test pbc_dist(pos3, pos4, non_periodic_sys) == 6.0u"Å"
+    @test pbc_dist(pos5, pos6, non_periodic_sys) == 6.0u"Å"
+
+    
+    sys2 = periodic_system(coor_list2, box, fractional=true)
+    lj = LJParameters(epsilon=0.1,sigma=2.5,cutoff=3.5,shift=false)
+
+    @test EnergyEval.inter_component_energy(sys1, sys2, lj) ≈ -0.5382944918043817u"eV"
+
+    
+    @test EnergyEval.intra_component_energy(sys1, lj) ≈ -0.2597893542152926u"eV"
+    @test EnergyEval.intra_component_energy(sys2, lj) ≈ -0.00023134752229080925u"eV"
+
+
+
 end

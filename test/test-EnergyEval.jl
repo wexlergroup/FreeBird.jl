6dfbd1de316a87fabf1c6eed613a530dc1d41e62
junchichen21 <c.junchi@wustl.edu>
2024-09-10 14:08:08 -0500

    feat: Add tests for inter_component_energy(), intra_component_energy(), and frozen_energy() functions

--- test/test-EnergyEval.jl
+++ test/test-EnergyEval.jl
@@ -31,12 +31,27 @@
     sys2 = periodic_system(coor_list2, box, fractional=true)
     lj = LJParameters(epsilon=0.1,sigma=2.5,cutoff=3.5,shift=false)
 
+    # Test inter_component_energy() function for calculating the energy between two components of a system
     @test EnergyEval.inter_component_energy(sys1, sys2, lj) ≈ -0.5382944918043817u"eV"
 
-    
+    # Test intra_component_energy() function for calculating the energy within a component of a system
     @test EnergyEval.intra_component_energy(sys1, lj) ≈ -0.2597893542152926u"eV"
     @test EnergyEval.intra_component_energy(sys2, lj) ≈ -0.00023134752229080925u"eV"
 
+
+    coor_list3 = [:H => [0.2, 0.2, 0.3], :H => [0.4, 0.4, 0.3], :H => [0.4, 0.4, 0.6], :H => [0.6, 0.6, 0.6], :H => [0.6, 0.6, 0.9], :H => [0.8, 0.8, 0.9]]
+    sys3 = periodic_system(coor_list3, box, fractional=true)
+    
+    @test EnergyEval.frozen_energy(sys3, lj, [2, 2, 2], [true, false, false]) ≈ -0.09978539310395718u"eV"
+
+    ljs1 = [LJParameters(epsilon=e) for e in [11, 21, 31, 12, 22, 32, 13, 23, 33]]
+    ljs2 = [LJParameters(epsilon=e) for e in [11, 12, 13, 22, 23, 33]]
+
+    ljp1 = CompositeLJParameters(3, ljs1)
+    ljp2 = CompositeLJParameters(3, ljs2)
+
+    @test EnergyEval.frozen_energy(sys3, ljp1, [2, 2, 2], [true, true, false]) ≈ -0.49376979751528466u"eV"
+    @test EnergyEval.frozen_energy(sys3, ljp2, [2, 2, 2], [true, true, false]) ≈ -0.4292804781822809u"eV"
 
 
 end

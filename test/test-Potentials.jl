1e4e29c74a38772bb427f01ddfba54e1eb9b95a2
junchichen21 <c.junchi@wustl.edu>
2024-09-14 17:23:47 -0500

    updated test code for Potential module. added test for CompositeLJParameters() function.

--- test/test-Potentials.jl
+++ test/test-Potentials.jl
@@ -1,5 +1,10 @@
 @testset "Lennard-Jones Potential" begin
     @test LJParameters() == LJParameters(1.0u"eV", 1.0u"Å", Inf, 0.0u"eV")
+    @test typeof(LJParameters().epsilon) == typeof(1.0u"eV")
+    @test typeof(LJParameters().sigma) == typeof(1.0u"Å")
+    @test typeof(LJParameters().cutoff) == Float64
+    @test typeof(LJParameters().shift) == typeof(0.0u"eV")
+
     @test LJParameters(epsilon=0.1,sigma=2.5,cutoff=3.5,shift=false) == LJParameters(0.1u"eV", 2.5u"Å", 3.5, 0.0u"eV")
 
     @test lj_energy(1.0u"eV", 1.0u"Å", 1.0u"Å") ≈ 0.0u"eV"
@@ -20,4 +25,9 @@
 
     v = [lj, lj, lj, lj, lj, lj, lj, lj, lj]
     @test CompositeLJParameters(3, v) |> typeof == CompositeLJParameters{3}
+
+    ljs = [LJParameters(epsilon=e) for e in [0.11, 0.21, 0.31, 0.12, 0.22, 0.32, 0.13, 0.23, 0.33]]
+
+    @test CompositeLJParameters(3, ljs) == CompositeLJParameters{3}(reshape(ljs, 3, 3))
+
 end

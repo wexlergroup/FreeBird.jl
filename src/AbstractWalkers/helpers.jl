dc5544dd53207b7197c6cd0193569fe522fc08e8
Ray Yang <mingrui.yang@outlook.com>
2024-09-24 10:56:48 -0500

    refactor: update `show` method for printing `LatticeSystem` to repl

--- src/AbstractWalkers/helpers.jl
+++ src/AbstractWalkers/helpers.jl
@@ -71,59 +71,15 @@ function print_lattice(io::IO, lattice::LatticeSystem{G}, boolvec::Vector{Bool})
     end
 end
 
-
-
-# function Base.show(io::IO, lattice::Vector{LatticeSystem{G}}) where G
-#     println(io, "Vector{LatticeSystem}(", length(lattice), "):")
-#     for (ind, l) in enumerate(lattice)
-#         println(io, "[", ind, "] LatticeSystem:")
-#         println(io, "    lattice_vectors      : ", l.lattice_vectors)
-#         println(io, "    positions            : ", l.positions)
-#         println(io, "    supercell_dimensions : ", l.supercell_dimensions)
-#         if G == SquareLattice
-#             println(io,  "    occupations          : "), print_square_lattice(io, l.supercell_dimensions, l.occupations)
-#             println(io,  "    adsorptions          : "), print_square_lattice(io, l.supercell_dimensions, l.adsorptions)
-#         elseif G == TriangularLattice
-#             println(io,  "    occupations          : "), print_triangular_lattice(io, l.supercell_dimensions, l.occupations)
-#             println(io,  "    adsorptions          : "), print_triangular_lattice(io, l.supercell_dimensions, l.adsorptions)
-#         else
-#             println(io, "    none-square none-triangular lattice")
-#             println(io, "    occupations          : ", l.occupations)
-#             println(io, "    adsorptions          : ", l.adsorptions)
-#         end
-#     end
-# end
-    
-
-# function Base.show(io::IO, lattice::LatticeSystem)
-#     println(io, "LatticeSystem:")
-#     println(io, "    lattice_vectors      : ", lattice.lattice_vectors)
-#     println(io, "    positions            : ", lattice.positions)
-#     println(io, "    supercell_dimensions : ", lattice.supercell_dimensions)
-#     println(io, "    occupations          : ", lattice.occupations)
-#     println(io, "    adsorptions          : ", lattice.adsorptions)
-#     println(io, "    neighbors            : ")
-#     if length(lattice.neighbors) > 10
-#         for i in 1:5
-#             println(io, "        site ", i, ": ", "nearest = ", lattice.neighbors[i][1], ", next-nearest = ", lattice.neighbors[i][2])
-#         end
-#         println(io, "        ⋮")
-#         for i in length(lattice.neighbors)-4:length(lattice.neighbors)
-#             println(io, "        site ", i, ": ", "nearest = ", lattice.neighbors[i][1], ", next-nearest = ", lattice.neighbors[i][2])
-#         end
-#     else
-#         for i in 1:length(lattice.neighbors)
-#             println(io, "        site ", i, ": ", "nearest = ", lattice.neighbors[i][1], ", next-nearest = ", lattice.neighbors[i][2])
-#         end
-#     end
-# end
-
 function Base.show(io::IO, lattice::LatticeSystem{G}) where G
     println(io, "LatticeSystem{$G}:")
     println(io, "    lattice_vectors      : ", lattice.lattice_vectors)
     println(io, "    positions            : ", lattice.positions)
     println(io, "    supercell_dimensions : ", lattice.supercell_dimensions)
     println(io, "    occupations          : "), print_lattice(io, lattice, lattice.occupations)
-    println(io, "    adsorptions          : "), print_lattice(io, lattice, lattice.adsorptions)
-    # println(io, "    neighbors            : ", lattice.neighbors)
+    if prod(lattice.adsorptions) == true
+        println(io, "    adsorptions          : full adsorption")
+    else
+        println(io, "    adsorptions          : "), print_lattice(io, lattice, lattice.adsorptions)
+    end
 end

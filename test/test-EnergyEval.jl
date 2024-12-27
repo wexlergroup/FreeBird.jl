3e6c03439ec186f6a2da9e5e57a4e27f3b2b2cda
junchichen21 <c.junchi@wustl.edu>
2024-12-26 18:48:42 -0600

    Completed the test code for functions in the EnergyEval.jl. Those of the functions in the helpers.jl still need to be added.

--- test/test-EnergyEval.jl
+++ test/test-EnergyEval.jl
@@ -1,71 +1,177 @@
-@testset "Energy Evaluation" begin
+@testset "Energy Evaluation Tests" begin
 
-    coor_list1 = [:H => [0.2, 0.5, 0.5], :H => [0.8, 0.5, 0.5], :H => [0.5, 0.2, 0.5], :H => [0.5, 0.8, 0.5], :H => [0.5, 0.5, 0.2], :H => [0.5, 0.5, 0.8]]
-    coor_list2 = [:H => [0, 0, 0], :H => [0.5, 0.5, 0.5]]
-    box = [[10.0, 0.0, 0.0], [0.0, 10.0, 0.0], [0.0, 0.0, 10.0]]u"Å"
+    @testset "EnergyEval.jl" begin
+        
+        # Generate different periodic systems
+        box = [[10.0, 0.0, 0.0], [0.0, 10.0, 0.0], [0.0, 0.0, 10.0]]u"Å"
 
-    sys1 = periodic_system(coor_list1, box, fractional=true)
-    pos1 = position(sys1.particles[1])
-    pos2 = position(sys1.particles[2])
+        coor_list1 = [:H => [0.2, 0.3, 0.5], :H => [0.8, 0.3, 0.5], :H => [0.5, 0.2, 0.3], :H => [0.7, 0.8, 0.3], :H => [0.5, 0.5, 0.1], :H => [0.4, 0.3, 0.1]]
+        sys1 = periodic_system(coor_list1, box, fractional=true)
 
-    pos3 = position(sys1.particles[3])
-    pos4 = position(sys1.particles[4])
-    
-    pos5 = position(sys1.particles[5])
-    pos6 = position(sys1.particles[6])
+        coor_list2 = [:H => [0, 0, 0.55], :H => [0.3, 0.6, 0.6], :H => [0.5, 0.5, 0.65]]
+        sys2 = periodic_system(coor_list2, box, fractional=true)
+        coor_list3 = [:H => [0.2, 0.2, 0.3], :H => [0.4, 0.4, 0.3], :H => [0.4, 0.4, 0.6], :H => [0.6, 0.6, 0.6], :H => [0.6, 0.6, 0.9], :H => [0.8, 0.8, 0.9]]
+        sys3 = periodic_system(coor_list3, box, fractional=true)
 
-    # Test output distances in x, y, z directions under the periodic condition
-    @test pbc_dist(pos1, pos2, sys1) == 4.0u"Å" 
-    @test pbc_dist(pos3, pos4, sys1) == 4.0u"Å"
-    @test pbc_dist(pos5, pos6, sys1) == 4.0u"Å"
+        # Generate single LJ potential.
+        lj = LJParameters(epsilon=0.1,sigma=2.5,cutoff=3.5,shift=false)
 
-    boundary_conditions = [DirichletZero(), DirichletZero(), DirichletZero()]
-    non_periodic_sys = FlexibleSystem(sys1; boundary_conditions=boundary_conditions)
+        # Generate composite LJ potential
+        ljs1 = [LJParameters(epsilon=e) for e in [11, 21, 31, 12, 22, 32, 13, 23, 33]]
+        ljs2 = [LJParameters(epsilon=e) for e in [11, 12, 13, 22, 23, 33]]
 
-    # Test output distances in x, y, z directions under the non-periodic condition
-    @test pbc_dist(pos1, pos2, non_periodic_sys) == 6.0u"Å"
-    @test pbc_dist(pos3, pos4, non_periodic_sys) == 6.0u"Å"
-    @test pbc_dist(pos5, pos6, non_periodic_sys) == 6.0u"Å"
+        ljp1 = CompositeLJParameters(3, ljs1)
+        ljp2 = CompositeLJParameters(3, ljs2)
 
-    
-    sys2 = periodic_system(coor_list2, box, fractional=true)
-    lj = LJParameters(epsilon=0.1,sigma=2.5,cutoff=3.5,shift=false)
+        
+        @testset "pbc_dist function" begin
 
-    # Test inter_component_energy() function for calculating the energy between two components of a system
-    @test EnergyEval.inter_component_energy(sys1, sys2, lj) ≈ -0.5382944918043817u"eV"
+            pos1 = position(sys1.particles[1])
+            pos2 = position(sys1.particles[2])
+            pos3 = position(sys1.particles[3])
+            pos4 = position(sys1.particles[4])
+            pos5 = position(sys1.particles[5])
+            pos6 = position(sys1.particles[6])
 
-    # Test intra_component_energy() function for calculating the energy within a component of a system
-    @test EnergyEval.intra_component_energy(sys1, lj) ≈ -0.2597893542152926u"eV"
-    @test EnergyEval.intra_component_energy(sys2, lj) ≈ -0.00023134752229080925u"eV"
+            # Test output distances in x, y, z directions under the periodic condition
+            @test pbc_dist(pos1, pos2, sys1) == 4.0u"Å"
+            @test pbc_dist(pos3, pos4, sys1) ≈ 4.472135u"Å"           rtol=1e-6
+            @test pbc_dist(pos5, pos6, sys1) ≈ 2.236068u"Å"           rtol=1e-6
 
 
-    coor_list3 = [:H => [0.2, 0.2, 0.3], :H => [0.4, 0.4, 0.3], :H => [0.4, 0.4, 0.6], :H => [0.6, 0.6, 0.6], :H => [0.6, 0.6, 0.9], :H => [0.8, 0.8, 0.9]]
-    sys3 = periodic_system(coor_list3, box, fractional=true)
-    
-    # Test frozen_energy() function for calculating the frozen energy in a system using the single LJ potential.
-    @test frozen_energy(sys3, lj, [2, 2, 2], [true, false, false]) ≈ -0.09978539310395718u"eV"
+            # Test output distances in x, y, z directions under the non-periodic condition
+            boundary_conditions = [DirichletZero(), DirichletZero(), DirichletZero()]
+            non_periodic_sys = FlexibleSystem(sys1; boundary_conditions=boundary_conditions)
 
-    ljs1 = [LJParameters(epsilon=e) for e in [11, 21, 31, 12, 22, 32, 13, 23, 33]]
-    ljs2 = [LJParameters(epsilon=e) for e in [11, 12, 13, 22, 23, 33]]
+            @test pbc_dist(pos1, pos2, non_periodic_sys) ≈ 6.0u"Å"
+            @test pbc_dist(pos3, pos4, non_periodic_sys) ≈ 6.324555u"Å"         rtol=1e-6
+            @test pbc_dist(pos5, pos6, sys1) ≈ 2.236068u"Å"                     rtol=1e-6
 
-    ljp1 = CompositeLJParameters(3, ljs1)
-    ljp2 = CompositeLJParameters(3, ljs2)
+        end
 
-    # Test frozen_energy() function for calculating the frozen energy in a system using the composite LJ potential.
-    @test frozen_energy(sys3, ljp1, [2, 2, 2], [true, true, false]) ≈ -0.49376979751528466u"eV"
-    @test frozen_energy(sys3, ljp2, [2, 2, 2], [true, true, false]) ≈ -0.4292804781822809u"eV"
+        @testset "Energy evaluation functions" begin
 
-    # Test whether interacting_energy() function is consistent with intra_component_energy() function when the single componenet system is not frozen
-    @test interacting_energy(sys3, lj) == EnergyEval.intra_component_energy(sys3, lj)
+            # Test inter_component_energy() function for calculating the energy between two components of a system
+            @test EnergyEval.inter_component_energy(sys1, sys2, lj) ≈ -0.2524365u"eV"        rtol=1e-6
 
-    # Test interacting_energy() function for calculating the interacting energy in a system using the single LJ potential.
-    @test interacting_energy(sys3, lj, [2, 2, 2], [true, false, false]) ≈ -0.46572785925144544u"eV"
+            # Test threaded implementation against non-threaded
+            EnergyEval.inter_component_energy(sys1, sys2, lj) ≈ EnergyEval.inter_component_energy_threaded(sys1, sys2, lj)
 
-    # Test interacting_energy() function for calculating the interacting energy in a system using the composite LJ potential.
-    @test interacting_energy(sys3, ljp1, [2, 2, 2], [true, false, false]) ≈ -0.6848119435000306u"eV"
-    @test interacting_energy(sys3, ljp2, [2, 2, 2], [true, false, false]) ≈ -0.6848119435000306u"eV"
+            # Test intra_component_energy() function for calculating the energy within a component of a system
+            @test EnergyEval.intra_component_energy(sys1, lj) ≈ 0.617334u"eV"               rtol=1e-6
+            @test EnergyEval.intra_component_energy(sys2, lj) ≈ 0.457052u"eV"               rtol=1e-6
+            
+            # Test frozen_energy() function for calculating the frozen energy in a system using the single LJ potential.
+            @test frozen_energy(sys3, lj, [2, 2, 2], [true, false, false]) ≈ -0.099785393u"eV"       rtol=1e-7
 
-    # Test for interacting_energy(lattice::LatticeSystem, h::LatticeGasHamiltonian)
+            # Test frozen_energy() function for calculating the frozen energy in a system using the composite LJ potential.
+            @test frozen_energy(sys3, ljp1, [2, 2, 2], [true, true, false]) ≈ -0.49376980u"eV"      rtol=1e-7
+            @test frozen_energy(sys3, ljp2, [2, 2, 2], [true, true, false]) ≈ -0.42928048u"eV"      rtol=1e-7
 
+            # Test whether interacting_energy() function is consistent with intra_component_energy() function when the single componenet system is not frozen
+            @test interacting_energy(sys3, lj) == EnergyEval.intra_component_energy(sys3, lj)
+
+            # Test interacting_energy() function for calculating the interacting energy in a system using the single LJ potential.
+            @test interacting_energy(sys3, lj, [2, 2, 2], [true, false, false]) ≈ -0.46572786u"eV"      rtol=1e-7
+
+            # Test interacting_energy() function for calculating the interacting energy in a system using the composite LJ potential.
+            @test interacting_energy(sys3, ljp1, [2, 2, 2], [true, false, false]) ≈ -0.68481194u"eV"    rtol=1e-7
+            @test interacting_energy(sys3, ljp2, [2, 2, 2], [true, false, false]) ≈ interacting_energy(sys3, ljp1, [2, 2, 2], [true, false, false])    # Shouldn't this test be false?
+
+            # Test for interacting_energy(lattice::LatticeSystem, h::LatticeGasHamiltonian) function
+            @test interacting_energy(sys3, lj) == EnergyEval.intra_component_energy(sys3, lj)
+            @test interacting_energy(sys3, lj) ≈ -0.56551325u"eV"            rtol=1e-7
+
+            empty_sys = periodic_system([], box, fractional=true)
+            @test interacting_energy(empty_sys, lj) == 0.0u"eV"
+
+        end
+
+        @testset "Energy evaluation functions for lattice models" begin
+
+            # Generate different Hamiltonians
+            h = LatticeGasHamiltonian(-1.0u"eV", 0.5u"eV", 0.2u"eV")
+            ham = GenericLatticeHamiltonian(-0.04u"eV", [-0.01, -0.0025]*u"eV")
+            hams = [GenericLatticeHamiltonian(-0.04, [-0.01, -0.0025], u"eV") for i in 1:4]
+            mlham = Hamiltonians.MLatticeHamiltonian(2, hams)
+
+            # Generate lattice
+            lattice = MLattice{1,SquareLattice}(
+                lattice_constant=1.0,
+                supercell_dimensions=(6,6,1),
+                periodicity = (true, true, false),
+                cutoff_radii = [1.1, 1.5],
+                components = [[1, 2, 4]],
+                adsorptions = [1, 2, 3] 
+            )
+            
+            # Setup square lattice with periodic boundaries
+            n_sites = 4
+            neighbors = [
+                [[2,4], [3]], # site 1's first and second neighbors
+                [[1,3], [4]], # site 2's first and second neighbors
+                [[2,4], [1]], # site 3's first and second neighbors
+                [[1,3], [2]]  # site 4's first and second neighbors
+            ]
+           
+
+            # Test for interacting_energy(lattice::SLattice, h::LatticeGasHamiltonian) function
+            empty_lattice = deepcopy(lattice)
+            empty_lattice.components[1] .= false
+            @test EnergyEval.interacting_energy(empty_lattice, ham) ≈ 0.0u"eV"
+            @test EnergyEval.interacting_energy(empty_lattice, mlham) ≈ 0.0u"eV"
+
+            single_site = deepcopy(lattice)
+            single_site.components[1] .= false
+            single_site.components[1][1] = true
+            @test EnergyEval.interacting_energy(single_site, ham) ≈ ham.on_site_interaction
+            @test EnergyEval.interacting_energy(single_site, mlham) ≈ ham.on_site_interaction
+
+            adjacent_sites = deepcopy(lattice)
+            adjacent_sites.components[1] .= false
+            adjacent_sites.components[1][1:2] .= true
+            @test EnergyEval.interacting_energy(adjacent_sites, ham) ≈ 2 * ham.on_site_interaction + ham.nth_neighbor_interactions[1]
+            @test EnergyEval.interacting_energy(adjacent_sites, mlham) ≈ 2 * ham.on_site_interaction + ham.nth_neighbor_interactions[1]
+
+            # Test for lattice_interaction_energy() function
+            empty_occupations = fill(false, n_sites)
+            @test EnergyEval.lattice_interaction_energy(empty_occupations, neighbors, ham) ≈ 0.0u"eV"
+
+            single_site = [true, false, false, false]
+            @test EnergyEval.lattice_interaction_energy(single_site, neighbors, ham) ≈ 0.0u"eV"
+
+            two_nn_sites = [true, true, false, false]
+            @test EnergyEval.lattice_interaction_energy(two_nn_sites, neighbors, ham) ≈ ham.nth_neighbor_interactions[1]
+        
+            two_nnn_sites = [true, false, true, false]
+            @test EnergyEval.lattice_interaction_energy(two_nnn_sites, neighbors, ham) ≈ ham.nth_neighbor_interactions[2]
+
+            full_lattice = fill(true, n_sites)
+            @test EnergyEval.lattice_interaction_energy(full_lattice, neighbors, ham) ≈ (2*ham.nth_neighbor_interactions[1] + 1*ham.nth_neighbor_interactions[2]) * 4 / 2
+
+            # Test for inter_component_energy() function
+            lattice1 = [true, false, false, false]
+            lattice2 = [false, true, false, false]
+            @test EnergyEval.inter_component_energy(lattice1, lattice2, neighbors, ham) ≈ ham.nth_neighbor_interactions[1]
+
+            lattice1 = [true, false, true, false]
+            lattice2 = [false, true, false, true]
+            @test EnergyEval.inter_component_energy(lattice1, lattice2, neighbors, ham) ≈ 4 * ham.nth_neighbor_interactions[1]
+
+            lattice1 = [true, false, false, false]
+            lattice2 = [true, true, true, true]
+            result1 = EnergyEval.inter_component_energy(lattice1, lattice2, neighbors, ham)
+            result2 = EnergyEval.inter_component_energy(lattice2, lattice1, neighbors, ham)
+            @test result1 == result2
+
+            # Test for interacting_energy() function
+
+        end
+
+    end
+
+    @testset "helpers.jl" begin
+        
+    end
 
 end

40a577555dd4ed0d05e95d58856684a261fd3450
junchichen21 <c.junchi@wustl.edu>
2024-12-31 18:07:23 -0600

    Changed "include("test-Hamiltonians.jl")" to "include("test-AbstractHamiltonians.jl")"

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,18 @@
 using FreeBird
 using Test
 
+using StaticArrays
 using DataFrames
 using Unitful
 using AtomsBase
 using LinearAlgebra
+
+include("test-AbstractHamiltonians.jl")
 
 include("test-AbstractWalkers.jl")
 
 include("test-AnalysisTools.jl")
 
 include("test-EnergyEval.jl")
-
-include("test-Hamiltonians.jl")
 
 include("test-Potentials.jl")

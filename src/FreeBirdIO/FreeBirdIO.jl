3a3eb1e633af68f4e2a9695d59daa141e72213d6
Ray Yang <mingrui.yang@outlook.com>
2025-01-29 14:50:26 -0600

    feat: add SaveFreePartEveryN strategy for saving free particles data

--- src/FreeBirdIO/FreeBirdIO.jl
+++ src/FreeBirdIO/FreeBirdIO.jl
@@ -17,7 +17,7 @@ export write_single_walker, write_walkers
 
 export append_system
 
-export DataSavingStrategy, SaveEveryN
+export DataSavingStrategy, SaveEveryN, SaveFreePartEveryN
 export write_df, write_df_every_n, write_walker_every_n, write_ls_every_n
 
 export generate_initial_configs
@@ -440,6 +440,28 @@ SaveEveryN is a concrete subtype of DataSavingStrategy that specifies saving dat
 end
 
 """
+    struct SaveFreePartEveryN <: DataSavingStrategy
+
+SaveFreePartEveryN is a concrete subtype of DataSavingStrategy that specifies saving data every N steps.
+Only the free particles are saved into the trajectory and snapshot files.
+
+# Fields
+- `df_filename::String`: The name of the file to save the DataFrame to.
+- `wk_filename::String`: The name of the file to save the atom walker to.
+- `ls_filename::String`: The name of the file to save the liveset to.
+- `n_traj::Int`: The number of steps between each save of the culled walker into a trajectory file.
+- `n_snap::Int`: The number of steps between each save of the liveset into a snapshot file.
+
+"""
+@kwdef struct SaveFreePartEveryN <: DataSavingStrategy
+    df_filename::String = "output_df.csv"
+    wk_filename::String = "output.traj.extxyz"
+    ls_filename::String = "output.ls.extxyz"
+    n_traj::Int = 100
+    n_snap::Int = 1000
+end
+
+"""
     write_df(filename::String, df::DataFrame)
 
 Write a DataFrame to a CSV file.
@@ -462,7 +484,7 @@ Write the DataFrame `df` to a file specified by `d_strategy.filename` every `d_s
 - `d_strategy::SaveEveryN`: The save strategy specifying the filename and the step interval.
 
 """
-function write_df_every_n(df::DataFrame, step::Int, d_strategy::SaveEveryN)
+function write_df_every_n(df::DataFrame, step::Int, d_strategy::DataSavingStrategy)
     if step % d_strategy.n_traj == 0
         write_df(d_strategy.df_filename, df)
     end
@@ -485,6 +507,13 @@ function write_walker_every_n(at::AtomWalker, step::Int, d_strategy::SaveEveryN)
     end
 end
 
+function write_walker_every_n(at::AtomWalker, step::Int, d_strategy::SaveFreePartEveryN)
+    if step % d_strategy.n_traj == 0
+        at = extract_free_par(at)
+        write_single_walker(d_strategy.wk_filename, at, true)
+    end
+end
+
 """
     write_ls_every_n(ls::AtomWalkers, step::Int, d_strategy::SaveEveryN)
 
@@ -499,6 +528,13 @@ Write the liveset `ls` to file every `n` steps, as specified by the `d_strategy`
 function write_ls_every_n(ls::AbstractLiveSet, step::Int, d_strategy::SaveEveryN)
     if step % d_strategy.n_snap == 0
         write_walkers(d_strategy.ls_filename, ls.walkers)
+    end
+end
+
+function write_ls_every_n(ls::AbstractLiveSet, step::Int, d_strategy::SaveFreePartEveryN)
+    if step % d_strategy.n_snap == 0
+        wks = extract_free_par.(ls.walkers)
+        write_walkers(d_strategy.ls_filename, wks)
     end
 end
 

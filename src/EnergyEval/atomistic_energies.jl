b5d1343298f9cb4bb338020f1f7f6b15288a2fdb
Ray Yang <mingrui.yang@outlook.com>
2025-02-01 16:58:12 -0600

    fix: update boundary condition handling in pbc_dist function

--- src/EnergyEval/atomistic_energies.jl
+++ src/EnergyEval/atomistic_energies.jl
@@ -15,13 +15,13 @@ Compute the distance between two positions considering periodic boundary conditi
 function pbc_dist(pos1::Union{SVector{T},Vector{T}},
                   pos2::Union{SVector{T},Vector{T}},
                   at::AbstractSystem) where {T}
-    pbc = at.boundary_conditions
-    box = at.bounding_box
+    pbc = periodicity(at)
+    box = cell_vectors(at)
     distsq = 0.0u"Å"^2
     for i in eachindex(pos1)
-        if pbc[i] == Periodic()
+        if pbc[i] == true
             distsq += min(abs(pos1[i] - pos2[i]), box[i][i] - abs(pos1[i] - pos2[i]))^2
-        elseif pbc[i] == DirichletZero()
+        elseif pbc[i] == false
             distsq += (pos1[i] - pos2[i])^2
         else
             error("Unsupported boundary condition: $(pbc[i])")

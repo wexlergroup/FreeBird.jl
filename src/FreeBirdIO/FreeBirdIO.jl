6a3b3dcaaa233ba0ad6776e074880c0529f30672
Ray Yang <mingrui.yang@outlook.com>
2025-03-07 13:13:01 -0600

    fix: correct frozen state assignment in convert_system_to_walker function

--- src/FreeBirdIO/FreeBirdIO.jl
+++ src/FreeBirdIO/FreeBirdIO.jl
@@ -61,7 +61,7 @@ function convert_system_to_walker(at::FlexibleSystem, resume::Bool)
     energy = (haskey(data, :energy) && resume) ? data[:energy]*u"eV" : 0.0u"eV"
     iter = (haskey(data, :iter) && resume && data[:iter]>=0) ? data[:iter] : 0
     list_num_par = (haskey(data, :list_num_par) && resume) ? data[:list_num_par] : [length(at)]
-    frozen = (haskey(data, :frozen) && resume) ? data[:frozen] : [false]
+    frozen = (haskey(data, :frozen) && resume) ? [data[:frozen]] : [false]
     e_frozen = (haskey(data, :energy_frozen_part) && resume) ? data[:energy_frozen_part]*u"eV" : 0.0u"eV"
     new_list = [Atom(atomic_symbol(i),position(i)) for i in at.particles]
     at = FastSystem(new_list, cell_vectors(at), periodicity(at))

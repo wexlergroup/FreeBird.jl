72ed6ff89df53ef8ea0d28e789b832ac3dde241a
Bennyboo0 <b.m.yazdi@wustl.edu>
2025-08-08 15:34:52 -0500

    trying to fix cell def

--- src/EnergyEval/atomistic_energies.jl
+++ src/EnergyEval/atomistic_energies.jl
@@ -126,7 +126,7 @@ function intra_component_energy(at::AbstractSystem, pot::AbstractPotential)
     pot_params = extract_gpu_params(pot)
     
     # 4. Unit cell matrix
-    cell = SMatrix{3,3,Float64}(ustrip.(at.cell))    
+    cell = SMatrix{3,3,Float64}(ustrip.(Matrix(at.cell)))
     # 5. Transfer to GPU
     pairs_gpu = CuArray(pairs)
     pos_gpu = CuArray(pos_cpu)

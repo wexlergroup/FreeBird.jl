b6291a38c8ee83d7791fd9ba309103e2b4a72e6c
Ray Yang <mingrui.yang@outlook.com>
2025-08-18 11:01:24 -0500

    feat: enhance MC_random_walk_lattice function with improved site occupation handling

--- src/MonteCarloMoves/random_walks.jl
+++ src/MonteCarloMoves/random_walks.jl
@@ -466,28 +466,40 @@ Perform a Monte Carlo random walk on the multi-component lattice system.
 - `lattice::MLattice{C,G}`: The proposed lattice after the random walk.
 """
 function MC_random_walk_lattice!(lattice::MLattice{C,G}) where {C,G}
+    # pick a random component to hop in
+    comp::Int = rand(1:C)
     # pick a random site to hop from
-    hop_from = rand(eachindex(lattice.components[comp]))
+    hop_from::Int = rand(eachindex(lattice.components[comp]))
     # pick a random site to hop to (can be the same as hop_from)
-    hop_to = rand(eachindex(lattice.components[comp]))
+    hop_to::Int = rand(eachindex(lattice.components[comp]))
+
+    # println("hop from: $hop_from, hop to: $hop_to, comp: $comp") # debug
     
     # case 1: occupied/unoccupied swap
-    is_occupied_from = any([lattice.components[comp][hop_from] for comp in 1:C])
-    is_occupied_to = any([lattice.components[comp][hop_to] for comp in 1:C])
+    is_occupied_from::Bool = any([lattice.components[comp][hop_from] for comp in 1:C])
+    is_occupied_to::Bool = any([lattice.components[comp][hop_to] for comp in 1:C])
+    # println("is_occupied_from: $is_occupied_from, is_occupied_to: $is_occupied_to") # debug
     if is_occupied_from != is_occupied_to # only swap if the occupation state changes
         # swap the occupation state of the sites
-        for comp in 1:C
+        for comp::Int in 1:C
             lattice.components[comp][hop_from], lattice.components[comp][hop_to] = 
             lattice.components[comp][hop_to], lattice.components[comp][hop_from]
         end
     # case 2: both sites occupied, swap components
     elseif is_occupied_from && is_occupied_to
         # find out which components are occupied at the sites
-        comp_from = findfirst(isequal(true), [lattice.components[comp][hop_from] for comp in 1:C])
-        comp_to = findfirst(isequal(true), [lattice.components[comp][hop_to] for comp in 1:C])
+        comp_from::Int = findfirst(isequal(true), [lattice.components[comp][hop_from] for comp in 1:C])
+        comp_to::Int = findfirst(isequal(true), [lattice.components[comp][hop_to] for comp in 1:C])
+        # println("comp_from: $comp_from, comp_to: $comp_to") # debug
         if comp_from != comp_to # only swap if the components are different
-            lattice.components[comp_from][hop_from], lattice.components[comp_to][hop_to] = 
-            lattice.components[comp_to][hop_to], lattice.components[comp_from][hop_from]
+            # first, unoccupy the site at comp_from
+            lattice.components[comp_from][hop_from] = !lattice.components[comp_from][hop_from]
+            # second, unoccupy the site at comp_to
+            lattice.components[comp_to][hop_from] = !lattice.components[comp_to][hop_from]
+            # third, occupy the site at comp_to
+            lattice.components[comp_to][hop_to] = !lattice.components[comp_to][hop_to]
+            # finally, occupy the site at comp_from
+            lattice.components[comp_from][hop_to] = !lattice.components[comp_from][hop_to]
         end
     end
     # case 3: both sites unoccupied, do nothing

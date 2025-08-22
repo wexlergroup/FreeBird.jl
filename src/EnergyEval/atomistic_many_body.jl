9b4ea863985f016bc65dca9436677b12fafa1679
Ray Yang <mingrui.yang@outlook.com>
2025-08-22 16:23:29 -0500

    refactor: update interacting_energy function to use Gupta potential and streamline energy calculations

--- src/EnergyEval/atomistic_many_body.jl
+++ src/EnergyEval/atomistic_many_body.jl
@@ -1,32 +1,18 @@
 function interacting_energy(at::AbstractSystem, pot::SingleComponentPotential{ManyBody})
-    # Calculate the energy from interactions between particles using the a single-component potential.
+    # Calculate the energy from interactions between particles using the Gupta potential.
     # The energy is calculated by summing the pairwise interactions between the free particles.
-    
-    # Build pairs of particles without double counting
-    pairs = Array{Tuple{Int,Int}, 1}()
-    for i in 1:length(at)
-        for j in (i+1):length(at)
-            push!(pairs, (i, j))
-        end
-    end
-    
-    energies_two_body = zeros(Float64, length(pairs))
-    energies_many_body = zeros(Float64, length(at))
-    
-    Threads.@threads for k in eachindex(pairs)
-        (i, j) = pairs[k]
-        r = pbc_dist(position(at, i), position(at, j), at)
-        energies_two_body[k] = ustrip(two_body_energy(r, pot))
-    end
-    
-    Threads.@threads for i in eachindex(at.position)
-        for j in eachindex(at.position)
+
+    pair_energies = zeros(Float64, length(at))
+    bond_energies = zeros(Float64, length(at))
+
+    Threads.@threads for i in 1:length(at)
+        for j in 1:length(at)
             if i != j
                 r = pbc_dist(position(at, i), position(at, j), at)
-                energies_many_body[i] += ustrip(many_body_energy(r, pot))
+                bond_energies[i] += ustrip(many_body_energy(r, pot))
+                pair_energies[i] += ustrip(two_body_energy(r, pot))
             end
         end
     end
-    # @show energies_two_body, energies_many_body # Debugging output to check energies
-    return total_energy(energies_two_body, energies_many_body, pot)
+    return total_energy(pair_energies, bond_energies, pot)
 end

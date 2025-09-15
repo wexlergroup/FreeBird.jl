6c05cfebdba48d721ef4b56fcb14577dc9786ddd
Ray Yang <mingrui.yang@outlook.com>
2025-09-15 10:54:36 -0500

    refactor: generalize single-site energy evaluation to arbitrary pair potentials

--- src/EnergyEval/atomistic_single_site.jl
+++ src/EnergyEval/atomistic_single_site.jl
@@ -1,5 +1,5 @@
 """
-    single_site_energy(index::Int, at::AbstractSystem, lj::LJParameters)
+    single_site_energy(index::Int, at::AbstractSystem, pot::SingleComponentPotential{Pairwise})
     single_site_energy(index::Int, at::AbstractSystem, cps::CompositeParameterSets{C,P}, list_num_par::Vector{Int})
     single_site_energy(index::Int, at::AbstractSystem, cps::CompositeParameterSets{C,P}, list_num_par::Vector{Int}, surface::AbstractSystem)
 
@@ -9,7 +9,7 @@ The energy is calculated by summing the pairwise interactions between the site a
 # Arguments
 - `index::Int`: The index of the site for which the energy is calculated.
 - `at::AbstractSystem`: The system for which the energy is calculated.
-- `lj::LJParameters`: The Lennard-Jones parameters.
+- `pot::SingleComponentPotential{Pairwise}`: The Lennard-Jones parameters.
 - `cps::CompositeParameterSets{C,P}`: The composite Lennard-Jones parameters.
 - `list_num_par::Vector{Int}`: The number of particles in each component.
 - `surface::AbstractSystem`: An optional surface system to consider in the energy calculation. See `LJSurfaceWalkers`.
@@ -20,7 +20,7 @@ The energy is calculated by summing the pairwise interactions between the site a
 """
 function single_site_energy(index::Int,
                             at::AbstractSystem, 
-                            lj::LJParameters,
+                            pot::SingleComponentPotential{Pairwise},
                             list_num_par::Vector{Int}
                             )
     
@@ -29,7 +29,7 @@ function single_site_energy(index::Int,
     energies = Array{typeof(0.0u"eV"), 1}(undef, length(all_index))
     Threads.@threads for i in eachindex(all_index)
         r = pbc_dist(position(at, index), position(at, all_index[i]), at)
-        energies[i] = lj_energy(r,lj)
+        energies[i] = pair_energy(r,pot)
     end
     return sum(energies)
 end
@@ -51,11 +51,11 @@ function single_site_energy(index::Int,
     Threads.@threads for i in eachindex(all_index)
         r = pbc_dist(position(at, index), position(at, all_index[i]), at)
         if all_index[i] in comp_split[from_comp]
-            energy = lj_energy(r,cps.param_sets[from_comp,from_comp])
+            energy = pair_energy(r,cps.param_sets[from_comp,from_comp])
             energies[i] = energy
         else
             to_comp = findfirst(x->all_index[i] in x, comp_split)
-            energy = lj_energy(r,cps.param_sets[from_comp,to_comp])
+            energy = pair_energy(r,cps.param_sets[from_comp,to_comp])
             energies[i] = energy
         end
     end
@@ -80,11 +80,11 @@ function single_site_energy(index::Int,
     Threads.@threads for i in eachindex(all_index)
         r = pbc_dist(position(at, index), position(at, all_index[i]), at)
         if all_index[i] in comp_split[from_comp]
-            energy = lj_energy(r,cps.param_sets[from_comp,from_comp])
+            energy = pair_energy(r,cps.param_sets[from_comp,from_comp])
             energies[i] = energy
         else
             to_comp = findfirst(x->all_index[i] in x, comp_split)
-            energy = lj_energy(r,cps.param_sets[from_comp,to_comp])
+            energy = pair_energy(r,cps.param_sets[from_comp,to_comp])
             energies[i] = energy
         end
     end
@@ -93,7 +93,7 @@ function single_site_energy(index::Int,
     Threads.@threads for i in eachindex(surface.position)
         r = pbc_dist(position(at, index), position(surface, i), at)
         to_comp = C # surface is the last component
-        energies_surface[i] = lj_energy(r,cps.param_sets[from_comp,to_comp])
+        energies_surface[i] = pair_energy(r,cps.param_sets[from_comp,to_comp])
     end
     external_e = sum(energies_surface)
     return internal_e + external_e

8c1eda424efadeeebcc41cd9746ed845d17c9dfe
Ray Yang <mingrui.yang@outlook.com>
2025-12-10 16:51:15 -0600

    Refactor MLIP model functions for MACE, ORB, and UMA; improve documentation and usage examples

--- docs/src/MLIPs.md
+++ docs/src/MLIPs.md
@@ -2,7 +2,7 @@
 
 FreeBird.jl supports the use of machine learning interatomic potentials (MLIPs) by interfacing with the ASE (Atomic Simulation Environment) Python calculators. One can use your local Python environment or set up a Conda environment within Julia using `CondaPkg.jl`.
 
-## UMA
+## Fairchem UMA model
 
 For UMA, we recommend setting up a local Conda environment. For example:
 
@@ -20,18 +20,34 @@ using PreferenceTools
 # press the "]" key to enter Pkg mode
 pkg> preference add CondaPkg env=/full/path/to/uma_env
 ```
-Replace `/full/path/to/uma_env` with the actual path to your Conda environment. You only need to do this once.
+Replace `/full/path/to/uma_env` with the actual path to your Conda environment. You only need to do this once. Restart Julia after setting this preference. To verify that the correct Python environment is being used, you can run:
+
+```julia
+using PreferenceTools
+# press the "]" key to enter Pkg mode
+pkg> preference status
+```
+
+You may need to resolve the environment again using:
+
+```julia
+using CondaPkg
+# press the "]" key to enter Pkg mode
+pkg> conda resolve
+```
 
 Now, we can use UMA MLIPs in FreeBird.jl. Here is an example of setting up an `MLIPAtomWalkers` live set:
 
 ```julia
 using FreeBird
-mlp = PyMLPotential(model_type="uma", model_version="uma-s-1p1", task_name="omol", use_gpu=false)
+mlp = uma_model("omat", "uma-s-1p1", device="cuda")
 walkers = AtomWalker.(generate_initial_configs(10, 562.5, 6; particle_type=:Si))
 ls = MLIPAtomWalkers(walkers, mlp)
 ```
 
-## MACE
+The `omat` task name and `uma-s-1p1` model name are required arguments for loading the UMA model. One can pass additional keyword arguments to customize the model loading, for example, `device="cuda"`. See [Fairchem UMA documentation](https://fair-chem.github.io/) for more details on other settings and usage.
+
+## MACE models
 
 Similarly, for MACE, you can set up a Conda environment and direct CondaPkg.jl to use it. 
 
@@ -56,12 +72,10 @@ Now, we can use MACE MLIPs in FreeBird.jl. Here is an example of setting up an `
 ```julia
 using FreeBird
 # Create MACE potential using the "small" pre-trained model, you can supply your own model path as well
-mlp = PyMLPotential(model_type="mace", model_version="small", float_type="float32", use_gpu=false)
-walkers = AtomWalker.(generate_initial_configs(10, 562.5, 6; particle_type=:Si))
-ls = MLIPAtomWalkers(walkers, mlp)
+mlp = mace_model(model_version="small", detype="float32", enable_cueq=true)
 ```
 
-## ORB
+## ORB models
 Again, you can point CondaPkg.jl to your local Conda environment where you have installed ORB. 
 Alternatively, you can install the required packages within your Julia environment using CondaPkg.jl:
 
@@ -74,8 +88,7 @@ pkg> conda pip_add orb-models
 Now, we can use ORB MLIPs in FreeBird.jl. Here is an example of setting up an `MLIPAtomWalkers` live set:
 
 ```julia
-using FreeBird
-mlp = PyMLPotential(model_type="orb", model_version="n", float_type="float32-high", use_gpu=false)
+mlp = orb_model(precision="float32-high", device="cuda")
 ```
 
 ## Add other MLIPs
@@ -86,16 +99,13 @@ Specially, in `src/AbstractPotentials/ase_calculators.jl`, you can add a new fun
 Using MACE as an example, you can create a new potential as follows:
 
 ```julia
-function PyMLPotential(;model_version::String, 
-                        float_type::String="",
-                        use_gpu::Bool=false)
-    # import relevant Python module
-    mace = pyimport("mace.calculators")
-    # create MACE ASE calculator using an appropriate function call
-    mace_calc = mace.mace_mp(model=model_version, default_dtype=float_type, enable_cueq=use_gpu)
-    # wrap and return as PyMLPotential
-    return PyMLPotential(ASEcalculator(mace_calc))
-end
+using FreeBird, PythonCall, ASEconvert
+# import relevant Python module
+mace = pyimport("mace.calculators")
+# create MACE ASE calculator using an appropriate function call
+mace_calc = mace.mace_mp(model_version="small", enable_cueq=true)
+# wrap and return as PyMLPotential
+mlp = PyMLPotential(ASEcalculator(mace_calc))
 ```
 FreeBird.jl will then be able to use this new MLIP for evaluating energies, which is defined in the `FreeBird.EnergyEval` module.
 Such as:
@@ -108,13 +118,13 @@ end
 
 ## Using MLIPs for nested sampling
 
-Here, we provide an example of using ORB MLIP for nested sampling with `MLIPAtomWalkers` live set.
+Here, we provide an example of using MACE MLIP for nested sampling with `MLIPAtomWalkers` live set.
 
 ```julia
 using FreeBird
 
-# Set up ORB potential
-mlp = PyMLPotential(model_type="orb", model_version="n", float_type="float32-high", use_gpu=false)
+# Set up MACE potential
+mlp = mace_model(model_version="small", detype="float32", enable_cueq=true)
 
 # Generate initial configurations and set up walkers, let's say 10 walkers of 6 Si atoms in a box
 walkers = AtomWalker.(generate_initial_configs(10, 562.5, 6; particle_type=:Si))
--- src/AbstractPotentials/ase_calculators.jl
+++ src/AbstractPotentials/ase_calculators.jl
@@ -6,75 +6,41 @@ can be passed around in Julia code.
 
 The wrapped calculator *must* be able to return energies in *electron-volts* via
 `atoms.get_potential_energy()`.
+
+# Available Models
+- MACE: see [`mace_model`](@ref) function)
+- ORB: see [`orb_model`](@ref) function)
+- UMA: see [`uma_model`](@ref) function)
+- CHGNet: see [`chgnet_model`](@ref) function)
 """
 struct PyMLPotential <: SingleComponentPotential{ManyBody}
     calc::ASEcalculator
 end
 
-function PyMLPotential(;model_type::String, 
-                        model_version::String, 
-                        float_type::String="",
-                        task_name::String="",
-                        inference_settings::Union{String,Dict}="default",
-                        use_gpu::Bool=false)
-    os = pyimport("os")
-    os.environ["KMP_DUPLICATE_LIB_OK"]=raw"True"
+"""
+    mace_model(arg...; kwargs...) <: PyMLPotential
 
-    # Mace
-    if model_type == "mace"
-        mace = pyimport("mace.calculators")
-        mace_calc = mace.mace_mp(model=model_version, default_dtype=float_type, enable_cueq=use_gpu)
-        return PyMLPotential(ASEcalculator(mace_calc))
-    end
+Loading MACE foundation model Python module through ASE interface.
+See https://github.com/ACEsuit/mace-foundations for the available arguments and usage.
 
-    # Orb
-    if model_type == "orb"
-        if use_gpu
-            device = "cuda"
-        else
-            device = "cpu"
-        end
-
-        pretrained = pyimport("orb_models.forcefield.pretrained")
-        ORBCalculator = pyimport("orb_models.forcefield.calculator").ORBCalculator
-        orbff = pretrained.orb_v3_direct_20_omat(
-            device=device,
-            precision=float_type
-        )
-        orb_calc = ORBCalculator(orbff, device=device)
-        return PyMLPotential(ASEcalculator(orb_calc))
-    end
-
-    # UMA
-    if lowercase(model_type) == "uma" 
-        if task_name == ""
-            error("Currently, using UMA MLIP model need specify the task_name.")
-        end
-
-        if use_gpu
-            device = "cuda"
-        else
-            device = "cpu"
-        end
-
-        uma_pretrained = pyimport("fairchem.core.calculate.pretrained_mlip")
-        uma_ase_calc = pyimport("fairchem.core.calculate.ase_calculator").FAIRChemCalculator
-        
-        predictor = uma_pretrained.get_predict_unit(model_name=model_version, 
-                                                    inference_settings=inference_settings, 
-                                                    device=device)
-        uma = uma_ase_calc(predictor, task_name=task_name)
-        
-        return PyMLPotential(ASEcalculator(uma))
-    end
-end
-
+# Example
+mlp = mace_model(model_version="small", detype="float32", enable_cueq=true)
+"""
 function mace_model(arg...; kwargs...)
     mace = pyimport("mace.calculators")
     mace_calc = mace.mace_mp(arg...; kwargs...)
     return PyMLPotential(ASEcalculator(mace_calc))
 end
 
+"""
+    orb_model(arg...; kwargs...) <: PyMLPotential
+
+Loading ORB potential Python module through ASE interface.
+See https://github.com/orbital-materials/orb-models for the available arguments and usage.
+
+# Example
+mlp = orb_model(precision="float32-high", device="cuda")
+"""
 function orb_model(arg...; kwargs...)
     pretrained = pyimport("orb_models.forcefield.pretrained")
     ORBCalculator = pyimport("orb_models.forcefield.calculator").ORBCalculator
@@ -83,34 +49,38 @@ function orb_model(arg...; kwargs...)
     return PyMLPotential(ASEcalculator(orb_calc))
 end
 
-function uma_model(arg...; kwargs...)
+"""
+    uma_model(task_name::String, model_name::String; kwargs...) <: PyMLPotential
+
+Loading Fairchem UMA model Python module through ASE interface.
+See https://fair-chem.github.io/ for the available arguments and usage.
+`task_name` and `model_name` are required.
+
+# Example
+mlp = uma_model("omat", "uma-s-1p1", device="cuda")
+"""
+function uma_model(task_name::String, model_name::String; kwargs...)
     uma_pretrained = pyimport("fairchem.core.calculate.pretrained_mlip")
     uma_ase_calc = pyimport("fairchem.core.calculate.ase_calculator").FAIRChemCalculator
-    predictor = uma_pretrained.get_predict_unit(arg...; kwargs...)
-    uma = uma_ase_calc(predictor)
+
+    predictor = uma_pretrained.get_predict_unit(model_name=model_name; kwargs...)
+    uma = uma_ase_calc(predictor, task_name=task_name)
     return PyMLPotential(ASEcalculator(uma))
 end
 
+"""
+    chgnet_model(arg...; kwargs...) <: PyMLPotential
+
+Loading CHGNet model Python module through ASE interface.
+See https://chgnet.lbl.gov/ for the available arguments and usage.
+
+# Example
+mlp = chgnet_model(model_name="r2scan", use_device="cuda")
+"""
 function chgnet_model(arg...; kwargs...)
     model = pyimport("chgnet.model.model")
     pot = model.CHGNet.load(arg...; kwargs...)
     calc = pyimport("chgnet.model.dynamics").CHGNetCalculator(potential=pot, property="energy")
     return PyMLPotential(ASEcalculator(calc))
-end
-
-
-"""
-    ASELennardJones(;epsilon=1.0, sigma=1.0, cutoff=Inf)
-
-Convenience constructor that creates an `ase.calculators.lj.LennardJones`
-calculator with the supplied parameters and wraps it in an `ASECalculator`.
-All energy/length units follow the ASE convention (eV / Å).
-"""
-function ASELennardJones(; epsilon = 1.0, sigma = 1.0, cutoff = Inf)
-    lj_mod = pyimport("ase.calculators.lj")
-    # ASE uses the keyword `rc` for the cut-off radius
-    rc = isfinite(cutoff) ? cutoff * sigma : cutoff
-    py_calc = lj_mod.LennardJones(; epsilon = epsilon, sigma = sigma, rc = rc)
-    return ASECalculator(py_calc)
 end
 

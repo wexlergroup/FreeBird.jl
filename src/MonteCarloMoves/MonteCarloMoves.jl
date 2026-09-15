253b569b14bbbeee6834b10473c88eb016c577a4
Eyob Tewelde <eyob@wustl.edu>
2026-09-15 18:43:45 -0500

    chore(atomic-lattice): remove unrelated module churn

--- src/MonteCarloMoves/MonteCarloMoves.jl
+++ src/MonteCarloMoves/MonteCarloMoves.jl
@@ -11,10 +11,6 @@ using Distributions
 using Unitful
 using StaticArrays
 
-# `pyconvert`, for reading an adsorbate position off the ASE frame in
-# lattice_random_walk!(::AtomicLattice).
-using PythonCall
-
 using ..AbstractPotentials
 using ..AbstractHamiltonians
 using ..AbstractWalkers
